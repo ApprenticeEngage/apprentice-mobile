80299a49d8234c11ecc4c16aae119c72988c9f6a
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-09 20:51:07 +0500

    added recommended for you section to homepage

--- src/components/CourseTile.tsx
+++ src/components/CourseTile.tsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import { View , Text} from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
 interface CourseProps {
-    title: string,
-    price: number,
-    recommendations: number,
-    author: string
+  title: string;
+  price: number;
+  recommendations: number;
+  author: string;
+  horizontal: boolean;
 }
 
-const CourseTile:React.FC<CourseProps> = ({title, price, recommendations, author}) => {
+const CourseTile: React.FC<CourseProps> = ({
+  title,
+  price,
+  recommendations,
+  author,
+  horizontal,
+}) => {
+  const viewClassName = `rounded-lg h-[200px] w-[350px] mb-3  bg-card-color ${
+    horizontal ? "ml-3" : ""
+  }`;
+
   return (
-    <View className="rounded-lg h-[200px] w-[350px] mb-4 bg-card-color">
+    <View className={viewClassName}>
       <View className="rounded-t-lg bg-card-pic h-[75px]"></View>
       <View className="flex-1 justify-center pl-6 mt-1 items-start">
         <Text className="text-[20px] font-bold text-center mb-2">{title}</Text>
         <Text className="text-[17px]">Price: {price}</Text>
-        <Text className="text-[17px]"># of Recommendations: {recommendations}</Text>
+        <Text className="text-[17px]">
+          # of Recommendations: {recommendations}
+        </Text>
         <Text className="text-[17px]">Author: {author}</Text>
       </View>
     </View>
   );
-}
+};
 
-export default CourseTile
+export default CourseTile;
--- src/screens/homepage.tsx
+++ src/screens/homepage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import navButton from "../components/navButton";
 import { Data } from "../contants/data";
@@ -9,49 +9,71 @@ import CourseTile from "../components/CourseTile";
 
 const Homepage = () => {
   return (
-    <View className="p-3 bg-bg-light flex-1 justify-start content-center min-h-screen w-screen">
-      <Text className="max-w-[240px] text-[30px] pt-6 pl-[13px]">
-        Find your mentor and Learn a Skill!
-      </Text>
-      {/* Search bar */}
-      <View className="rounded-lg h-[50px] ml-2 mt-4 w-[350px] bg-primary-accent flex-row justify-start items-center">
-        <View className="pl-7">
-          <Ionicons className="" name={"radio-button-off"} size={32} />
+    <ScrollView className="flex-1">
+      <View className="p-3 bg-bg-light flex-1 justify-start content-center min-h-screen w-screen">
+        <Text className="max-w-[240px] text-[30px] pt-6 pl-[13px]">
+          Find your mentor and Learn a Skill!
+        </Text>
+        {/* Search bar */}
+        <View className="rounded-lg h-[50px] ml-2 mt-4 w-[350px] bg-primary-accent flex-row justify-start items-center">
+          <View className="pl-7">
+            <Ionicons className="" name={"radio-button-off"} size={32} />
+          </View>
+          <Text className=" text-center pl-6 text-xl">Search</Text>
         </View>
-        <Text className=" text-center pl-6 text-xl">Search</Text>
-      </View>
 
-      <View className="flex-row justify-between items-center">
-        <Text className="max-w-[300px] text-[22px] pt-6 pl-[10px]">
-          Discover Vocations
+        <View className="flex-row justify-between items-center">
+          <Text className="max-w-[300px] text-[22px] pt-6 pl-[10px]">
+            Discover Vocations
+          </Text>
+          <Text className="max-w-[300px] underline text-[14px] pt-7 pr-3">
+            View All
+          </Text>
+        </View>
+        <View className="mb-4">
+          <FlatList
+            data={Data}
+            horizontal={true}
+            renderItem={({ item }) => <CourseCategory title={item.title} />}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+        <View className="flex-1 items-center">
+          <FlatList
+            data={courseData}
+            renderItem={({ item }) => (
+              <CourseTile
+                title={item.title}
+                price={item.price}
+                recommendations={item.recommendations}
+                author={item.author}
+                horizontal={false}
+              />
+            )}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+        <Text className="max-w-[300px] text-[24px] pt-1 pb-4 pl-[10px]">
+          Recommended For You
         </Text>
-        <Text className="max-w-[300px] underline text-[14px] pt-7 pr-3">
-          View All
-        </Text>
+        <View className="flex-1 items-center">
+          <FlatList
+            data={courseData}
+            horizontal={true}
+            renderItem={({ item }) => (
+              <CourseTile
+                title={item.title}
+                price={item.price}
+                recommendations={item.recommendations}
+                author={item.author}
+                horizontal={true}
+              />
+            )}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
       </View>
-      <View className="mb-4">
-        <FlatList
-          data={Data}
-          horizontal={true}
-          renderItem={({ item }) => <CourseCategory title={item.title} />}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
-      <View className="flex-1 items-center">
-        <FlatList
-          data={courseData}
-          renderItem={({ item }) => (
-            <CourseTile
-              title={item.title}
-              price={item.price}
-              recommendations={item.recommendations}
-              author={item.author}
-            />
-          )}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 

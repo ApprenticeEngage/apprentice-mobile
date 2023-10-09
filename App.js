0a410d812be78cf97a8377065d2f932c7433ab78
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-09 18:01:08 +0500

    added fonts to app.js; not configured tailwind yet; color cofnig added

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { setCustomText } from "react-native-global-props";
+import * as Font from "expo-font";
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "Assistant-Bold": require("./assets/fonts/Assistant-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Texts}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text  className="bg-primary-green text-[40px]">
+        Hello World regarding today
+      </Text>
     </View>
   );
 }
@@ -13,8 +40,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Texts: {
+    fontFamily: "Assistant-Bold"
   },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        assistant: ["Assistant-ExtraBold"],
+      },
+    },
+    colors: {
+      "card-color": "#B8B56B",
+      "bg-light": "#ECE9F6",
+      "primary-green": "#98B847",
+      "primary-accent": "#AADF26",
+      "card-pic": "#824057",
+    },
   },
   plugins: [],
 };
-

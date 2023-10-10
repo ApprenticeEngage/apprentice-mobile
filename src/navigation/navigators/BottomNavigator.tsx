40adde560d480fd90e820d96eaaf00a049c44372
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-10 20:46:15 +0500

    updated bottom navigator

--- src/navigation/navigators/BottomNavigator.tsx
+++ src/navigation/navigators/BottomNavigator.tsx
@@ -1,5 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Homepage from "../../screens/homepage";
+import { Header, HeaderTitle } from "@react-navigation/elements";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const options = {
@@ -11,8 +13,29 @@ const options = {
 
 function TabNavigator() {
   return (
-    <Tab.Navigator screenOptions={options}>
-      <Tab.Screen name="Home" component={Homepage} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveBackgroundColor: "#D8D0EC",
+        tabBarLabelStyle: {
+          marginBottom: 7,
+          color: "#000000",
+          fontSize: 13,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Homepage}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Ionicons name="home-sharp" size={24} color={"black"} />
+            ) : (
+              <Ionicons name="home-outline" size={24} color={"red"} />
+            ),
+        }}
+      />
     </Tab.Navigator>
   );
 }

9122acdb7a8b6daab352cf7559867e1f3ddfba33
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-13 20:41:26 +0500

    added section tiles

---
+++ src/components/course-landing/SectionTile.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, StyleSheet } from "react-native";
+import MyText from "../MyText";
+import Title from "./Title";
+import SubTitle from "./subTitle";
+import { ScrollView } from "react-native-gesture-handler";
+
+
+const SectionTile = () => {
+  return (
+    <div>SectionTile</div>
+  )
+}
+
+export default SectionTile
